9a78156d07926f568eb90849f3dad214cc73a223
Kameron Seto <tan@Sad-Panda.local>
2018-10-15 19:59:10 -0700

    rotate/gravity feature in progress

--- challenge_1/client/scripts/app.js
+++ challenge_1/client/scripts/app.js
@@ -4,7 +4,7 @@ const state = {
   // 0 = blank, 1 = player X, 2 = player O
   matrix: [[0,0,0],[0,0,0],[0,0,0]],
   currentPlayer: 1,
-  // twistyMode: false,
+  twistyMode: false,
   movesLeft: 9,
   isGameEnded: false,
   lastWinner: 1,
@@ -41,7 +41,7 @@ const state = {
     state.tally = {1: 0, 2: 0};
   },
 
-  isWinnerFound: function () {
+  _isWinner: function (playerNum) {
     // checks the board for a winner; returns true/false
     // there are only 8 ways to win: 3 rows, 3 columns, 2 diagonals
     // just check all of them
@@ -49,29 +49,58 @@ const state = {
 
     // check all rows
     for (let r = 0; r < 3; r++) {
-      if (M[r][0] === M[r][1] && M[r][0] === M[r][2] && M[r][0] !== 0) {
+      if (M[r][0] === M[r][1] && M[r][0] === M[r][2] && M[r][0] === playerNum) {
         return true;
       }
     }
 
     // check all columns
     for (let c = 0; c < 3; c++) {
-      if (M[0][c] === M[1][c] && M[0][c] === M[2][c] && M[0][c] !== 0) {
+      if (M[0][c] === M[1][c] && M[0][c] === M[2][c] && M[0][c] === playerNum) {
         return true;
       }
     }
 
     // diagonal 1
-    if (M[0][0] === M[1][1] && M[0][0] === M[2][2] && M[0][0] !== 0) {
+    if (M[0][0] === M[1][1] && M[0][0] === M[2][2] && M[0][0] === playerNum) {
       return true;
     }
 
     // diagonal 2
-    if (M[0][2] === M[1][1] && M[1][1] === M[2][0] && M[0][2] !== 0) {
+    if (M[0][2] === M[1][1] && M[1][1] === M[2][0] && M[0][2] === playerNum) {
       return true;
+    }    
+  },
+
+  findWinner: function () {
+    // normally there can only be 1 winner
+    // let's make the assumption that with rotation and gravity, there can be 2 winners
+    // in which case it's a tie
+    if (state._isWinner(1)) {
+      if (state._isWinner(2)) {
+        return [1,2];
+      }
+      return [1];
     }
 
-    return false;
+    if (state._isWinner(2)) {
+      return [2];
+    }
+
+    return null;
+  },
+
+  // clockwise 90 degrees
+  rotateMatrixBy90AddGravity: function () {
+    const copy = JSON.parse(JSON.stringify(state.matrix));
+    matrix[0][0] = copy[2][0];
+    matrix[0][1] = copy[1][0];
+    matrix[0][2] = copy[0][0];
+    matrix[1][0] = copy[2][1];
+    matrix[1][2] = copy[0][1];
+    matrix[2][0] = copy[2][2];
+    matrix[2][1] = copy[1][2];
+    matrix[2][2] = copy[0][2];
   },
 }
 
@@ -93,13 +122,12 @@ const input = {
 
   handleNextRound: function () {
     document.getElementById('next').classList.add('hidden');
-    // document.getElementById('board').classList.add('rotated');
     state.resetBoard();
     render.renderBoard();
     render.renderMessage();
   },
 
-  handleTwisty: function () {
+  handleTwistyButton: function () {
     state.toggleTwisty();
     if (state.twistyMode) {
       document.getElementById('twistyMode').classList.add('on');
@@ -109,8 +137,9 @@ const input = {
   },
 
   // event.target is a jQuery thing!
+  // opportunity for refactoring here... multiple checks for winners, here
+  // and in the rendering portions
   handleClick: function (elem) {
-    // console.log(elem.id);
     const blank = Array(5).fill(String.fromCharCode(160)).join('');
 
     if (elem.innerText === blank && !state.isGameEnded) {
@@ -119,7 +148,7 @@ const input = {
       const col = elem.id[3];
       state.updateMatrix(row, col);
 
-      if (state.isWinnerFound()) {
+      if (state.findWinner()) {
         state.isGameEnded = true;
         state.lastWinner = state.currentPlayer;
         state.tally[state.currentPlayer]++;
@@ -128,7 +157,26 @@ const input = {
         state.isGameEnded = true;
         document.getElementById('next').classList.remove('hidden');
       } else {
-        state.togglePlayer();
+        if (twistyMode) {
+          document.getElementById('board').classList.add('rotated');
+          state.rotateMatrixBy90AddGravity();
+          //render.renderBoard();
+
+          const winners = state.findWinner();
+          if (winners) {
+            state.isGameEnded = true;
+            document.getElementById('next').classList.remove('hidden');
+
+            if (winners.length === 1) {
+              state.lastWinner = winners[0];
+              state.tally[winners[0]]++;
+            }
+          } else {
+            state.togglePlayer();
+          }
+        } else {
+          state.togglePlayer();
+        }
       }
 
       render.renderBoard();
@@ -151,8 +199,14 @@ const render = {
 
   renderMessage: function () {
     if (state.isGameEnded) {
-      if (state.isWinnerFound()) {
-        document.getElementById('message').innerText = `You won, player ${state.playerMap[state.currentPlayer]}!`;
+      const winners = state.findWinner();
+
+      if (winners) {
+        if (winners.length === 1) {
+          document.getElementById('message').innerText = `You won, player ${state.playerMap[winners[0]]}!`;
+        } else {
+          document.getElementById('message').innerText = 'It\'s a draw!';  
+        }
       } else {
         document.getElementById('message').innerText = 'It\'s a draw!';
       }
