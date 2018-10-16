20047fae3789bbe68c2d4df7eac2115802d3c441
Kameron Seto <tan@Sad-Panda.local>
2018-10-16 12:21:47 -0700

    index.html done, server.js initial setup

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,25 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const port = 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, './client')));
+
+app.post('/', (req, res) => {
+  // JSON to CSV logic
+  // sends CSV & form
+  console.log(req.body);
+});
+
+app.listen(port, () => {
+  console.log(`Listening at port ${port}`);
+});
 
 
+// app.get('/', (req, res) => {
+//   //res.sendFile(path.join(__dirname, 'client/index.html'));
+// });
