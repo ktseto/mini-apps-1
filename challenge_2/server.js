52bb1cd3d06c249fd8840130dfe17f2ea9b3b16f
Kameron Seto <tan@Sad-Panda.local>
2018-10-16 14:40:47 -0700

    testing response with static data

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -7,12 +7,36 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, './client')));
+app.use(express.static('client'));
 
 app.post('/', (req, res) => {
   // JSON to CSV logic
   // sends CSV & form
-  console.log(req.body);
+  //console.log(req.body);
+  const input = req.body.jsonString;
+
+  const colUnique = {firstName: true, lastName: true, age: true};
+  const obj1 = {firstName: 'John', lastName: 'Smith'};
+  const obj2 = {firstName: 'Jane', lastName: 'Doe', age: 30};
+  const objList = [obj1, obj2];
+
+  const colNames = ['firstName', 'lastName', 'age'];
+  let csv = objList.map((obj) => {
+    return colNames.map((col) => obj[col]).join(',');
+  }).join('\n');
+
+  csv = colNames.join(',') + '\n' + csv;
+  
+  const template = `
+    <form method="post">
+      <label for="jsonString">Enter JSON string:</label>
+      <div><textarea id="jsonString" name="jsonString"></textarea></div>
+      <input type="submit">
+    </form>
+    <p>${csv}</p>
+  `;
+
+  res.send(template);
 });
 
 app.listen(port, () => {
