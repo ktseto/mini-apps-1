8676bcff3c828d49e1ec2d89fd97485a7f4e6fd2
Kameron Seto <tan@Sad-Panda.local>
2018-10-18 08:48:58 -0700

    account creation step working from client to server

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,74 +1,138 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { step: 'Home' };
+    this.state = {
+      currentStep: 'Checkout',
+      checkout_id: null,
+    };
+    this.next = {
+      'Checkout': 'Account',
+      'Account': 'Contact',
+      'Contact': 'Billing',
+      'Billing': 'Confirm',
+      'Confirm': 'Checkout',
+    };
   }
 
-  handleHomeCheckOut() {
-    this.setState({ step: 'Account' });
-  }
+  handleNext(e) {
+    e.preventDefault();
 
-  handleAccountNext() {
-    this.setState({ step: 'Contact' });
-  }
+    const endpoint = e.target.id;
 
-  handleContactNext() {
-    this.setState({ step: 'Billing' });
-  }
-
-  handleBillingNext() {
-    this.setState({ step: 'Confirm' });
-  }
-
-  handleConfirmPurchase() {
-    this.setState({ step: 'Home' });
+    $.ajax({
+      url: '/' + endpoint,
+      method: 'POST',
+      data: `checkout_id=${this.state.checkout_id}&${$(e.target).serialize()}`,
+    })
+    .done((id) => {
+      // js doesn't like me using e.target.id in the line below...
+      // something about reusing event target and it's now null...
+      if (endpoint === 'checkout') {
+        this.setState({
+          currentStep: this.next[this.state.currentStep],
+          checkout_id: id,
+        });
+      } else {
+        this.setState({ currentStep: this.next[this.state.currentStep] });
+      }
+    });
   }
 
   // componentDidMount() {
-  //   this.setState({ step: 'Home' });
+  //   this.setState({ step: 'Checkout' });
   // }
 
   render() {
     return (
       <div>
-        <div><Home currentStep={this.state.step} handleHomeCheckOut={this.handleHomeCheckOut.bind(this)}/></div>
-        <div><Account currentStep={this.state.step} handleAccountNext={this.handleAccountNext.bind(this)}/></div>
-        <div><Contact currentStep={this.state.step} handleContactNext={this.handleContactNext.bind(this)}/></div>
-        <div><Billing currentStep={this.state.step} handleBillingNext={this.handleBillingNext.bind(this)}/></div>
-        <div><Confirm currentStep={this.state.step} handleConfirmPurchase={this.handleConfirmPurchase.bind(this)}/></div>
+        <div><Checkout currentStep={this.state.currentStep} handleNext={this.handleNext.bind(this)}/></div>
+        <div><Account currentStep={this.state.currentStep} handleNext={this.handleNext.bind(this)}/></div>
+        <div><Contact currentStep={this.state.currentStep} handleNext={this.handleNext.bind(this)}/></div>
+        <div><Billing currentStep={this.state.currentStep} handleNext={this.handleNext.bind(this)}/></div>
+        <div><Confirm currentStep={this.state.currentStep} handleNext={this.handleNext.bind(this)}/></div>
       </div>
     );
   }
 }
 
 
-const Home = ({ currentStep, handleHomeCheckOut }) => (
-  <button style={{display: currentStep === 'Home' ? 'block' : 'none'}}
-          onClick={handleHomeCheckOut}>
-  Check Out</button>
+// const Checkout = ({ currentStep, handleNext }) => (
+//   <div style={{display: currentStep === 'Checkout' ? 'block' : 'none'}}>
+//     <p>Welcome to your cart!</p>
+//     <button onClick={handleNext}>
+//     Check Out</button>
+//   </div>
+// );
+
+const Checkout = ({ currentStep, handleNext }) => (
+  <div style={{display: currentStep === 'Checkout' ? 'block' : 'none'}}>
+    <p>Welcome to your cart!</p>
+    <form id="checkout"
+          onSubmit={handleNext}
+          action="/checkout"
+          method="POST">
+      <input type="submit" value="Check out"/>
+    </form>
+  </div>
 );
 
-const Account = ({ currentStep, handleAccountNext }) => (
-  <button style={{display: currentStep === 'Account' ? 'block' : 'none'}}
-          onClick={handleAccountNext}>
-  Next</button>
-);
+// const Account = ({ currentStep, handleNext }) => (
+//   <div id="account"
+//        style={{display: currentStep === 'Account' ? 'block' : 'none'}}>
+//     <div>Name: <input type="text" id="name"/></div>
+//     <div>Email: <input type="text" id="email"/></div>
+//     <div>Password: <input type="text" id="password"/></div>
+//     <button onClick={handleNext}>Next</button>
+//   </div>
+// );
 
-const Contact = ({ currentStep, handleContactNext }) => (
+
+
+
+const Account = ({ currentStep, handleNext }) => (
+  <form id="account"
+        onSubmit={handleNext}
+        action="/account"
+        method="POST"
+        style={{display: currentStep === 'Account' ? 'block' : 'none'}}>
+    <div>Name: <input type="text" id="name" name="name"/></div>
+    <div>Email: <input type="text" id="email" name="email"/></div>
+    <div>Password: <input type="text" id="password" name="password"/></div>
+    <input type="submit" value="Next"/>
+  </form>
+);
+//<input type="submit" onClick={handleNext}/>
+//<button onClick={handleNext}>Next</button>
+
+//    <div>Email: <input type="text" id="email"/></div>
+//    <div>Password: <input type="text" id="password"/></div>
+// const Account = ({ currentStep, handleNext }) => (
+//   <form id="account"
+//         action="/account"
+//         method="POST"
+//         style={{display: currentStep === 'Account' ? 'block' : 'none'}}>
+//     <div>Name: <input type="text" id="name"/></div>
+//     <button id="accountButton" onClick={handleNext}>Next</button>
+//   </form>
+// );
+//<input type="submit" onClick={handleNext}/>  
+//
+
+const Contact = ({ currentStep, handleNext }) => (
   <button style={{display: currentStep === 'Contact' ? 'block' : 'none'}}
-          onClick={handleContactNext}>
+          onClick={handleNext}>
   Next</button>
 );
 
-const Billing = ({ currentStep, handleBillingNext }) => (
+const Billing = ({ currentStep, handleNext }) => (
   <button style={{display: currentStep === 'Billing' ? 'block' : 'none'}}
-          onClick={handleBillingNext}>
+          onClick={handleNext}>
   Next</button>
 );
 
-const Confirm = ({ currentStep, handleConfirmPurchase }) => (
+const Confirm = ({ currentStep, handleNext }) => (
   <button style={{display: currentStep === 'Confirm' ? 'block' : 'none'}}
-          onClick={handleConfirmPurchase}>
+          onClick={handleNext}>
   Purchase</button>
 );
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -10,6 +10,52 @@ app.use(express.static('compiled'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post('/checkout', (req, res) => {
+  console.log('checkout:',req.body);
+  db.queryAsync(`
+    INSERT INTO checkout (account_id, contact_id, billing_id, purchased)
+    VALUES (null, null, null, null)
+  `,
+    (err, okpacket) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(201).send(String(okpacket.insertId));
+      }
+    }
+  );
+});
+
+app.post('/account', (req, res) => {
+  db.queryAsync(`
+    INSERT INTO account (name, email, password)
+    VALUES ("${req.body.name}", "${req.body.email}", "${req.body.password}")
+  `,
+    (err, okpacket) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('account', okpacket.insertId);
+        console.log('account:',req.body);
+        db.queryAsync(`
+          UPDATE checkout SET account_id = ${okpacket.insertId}
+          WHERE id = ${req.body.checkout_id}
+        `,
+          (err, _) => {
+            if (err) {
+              console.log(err);
+            } else {
+              res.status(201).send(String(okpacket.insertId));
+            }
+          }
+        );
+      }
+    }
+  );
+});
+
+
+
 app.listen(port, () => {
   console.log('Listening on port ', port);
 });
